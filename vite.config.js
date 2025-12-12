9468f917d65ae70878f957a377242c4a7acd3236
CandriPanjaitan <candri.informatika.08@gmail.com>
2025-12-12 13:05:06 +0700

    deploy to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Server saat development (opsional)
   server: {
-    port: 5173,     // biar aman saat dev
+    port: 5173,
     open: true
   },
+
+  // Build untuk produksi (Vercel)
   build: {
-    outDir: 'dist', // wajib untuk Vercel
+    outDir: 'dist'
   }
 })
